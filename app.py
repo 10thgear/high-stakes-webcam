192e5ed3002b9afe7968206d6945c5a880b41d57
unknown <Jun Ye>
2025-01-17 00:54:55 -0800

    first working version

---
+++ app.py
@@ -0,0 +1,30 @@
+import cv2
+from ultralytics import YOLO
+
+model = YOLO('best.pt')
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Error: Could not open the camera.")
+    exit()
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # If frame reading was successful
+    if not ret:
+        print("Failed to grab frame.")
+        break
+
+    #results = model.track(frame, classes=0, conf=0.8, imgsz=480)
+    results = model.predict(frame, show_labels=False, show_conf=False)
+    cv2.imshow("Live Camera", results[0].plot())
+
+    # Press 'q' on the keyboard to exit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
