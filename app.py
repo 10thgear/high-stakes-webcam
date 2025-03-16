61e9fcab766e71108c3891631fbc1971fa30f4cc
Jun Ye <yetsun@gmail.com>
2025-03-15 22:34:41 -0700

    fix linter

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import cv2
 from ultralytics import YOLO
 
-model = YOLO('best.pt')
+model = YOLO("best.pt")
 cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
@@ -17,12 +17,12 @@ while True:
         print("Failed to grab frame.")
         break
 
-    #results = model.track(frame, classes=0, conf=0.8, imgsz=480)
+    # results = model.track(frame, classes=0, conf=0.8, imgsz=480)
     results = model.predict(frame, show_labels=False, show_conf=False)
     cv2.imshow("Live Camera", results[0].plot())
 
     # Press 'q' on the keyboard to exit
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 
